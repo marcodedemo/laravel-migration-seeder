8d8b56dcdd34c8c3f190a038e01c7b988501929a
marcodedemo <marco.dedemo@gmail.com>
2023-05-12 10:29:05 +0200

    feat: Controller query correction

--- app/Http/Controllers/Train/TrainController.php
+++ app/Http/Controllers/Train/TrainController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class TrainController extends Controller
 {
     public function index(){
-        $trains = Train::all()->sortBy('Data_partenza');
+        $trains = Train::all()->where('Data_partenza', '>=', now())->sortBy('Data_partenza');
 
         return view('home', compact('trains'));   
     }
